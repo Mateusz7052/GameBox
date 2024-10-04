262d94582ea76528e1f0427a1e5b471b78ad67d7
V-Karch <kluna0905@gmail.com>
2024-10-04 15:44:31 -0400

    TicTacToe: added Board class

---
+++ API/TicTacToe/Board.py
@@ -0,0 +1,41 @@
+from Tile import Tile
+from typing import List
+
+
+class Board:
+    """
+    Represents a Tic-Tac-Toe board.
+
+    The Board is initialized with a specified size and consists of a grid of tiles.
+    Each tile can be empty or contain a character representing a move by either player.
+
+    Attributes:
+        board_size (int): The size of the board (number of rows and columns).
+        total_tiles (float): The total number of tiles on the board.
+        tiles (List[List[Tile]]): A 2D list containing Tile objects representing the board.
+
+    Methods:
+        __str__(): Returns a string representation of the board for display.
+        __repr__(): Returns the same string representation as __str__().
+    """
+
+    def __init__(self, board_size: int):
+        self.board_size = board_size
+        self.total_tiles: float = board_size * board_size
+        self.tiles: List[List[Tile]] = []
+
+        for _ in range(self.board_size):
+            rows = []
+
+            for _ in range(self.board_size):
+                rows.append(Tile())
+
+            self.tiles.append(rows)
+
+    def __str__(self):
+        return "\n".join(
+            ["".join([str(tile) for tile in tile_row]) for tile_row in self.tiles]
+        )
+
+    def __repr__(self):
+        return self.__str__()
